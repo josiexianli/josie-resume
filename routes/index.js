b900c9adfc53ab97ee6ebe177d02d7c17836c2c8
Xian <josiexianli@gmail.com>
2018-01-17 17:56:05 -0800

    INIT

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Chris Yao Resume' });
+  res.render('index', { title: 'Xian Li Resume' });
 });
 
 module.exports = router;
