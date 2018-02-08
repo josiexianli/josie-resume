d418baa3d387a31da59a4df4e7348df865c891b0
Xian <josiexianli@gmail.com>
2018-02-07 17:48:55 -0800

    INIT

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var expressValidator = require('express-validator');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Xian Li Resume' });
 });
 
+// router.post('/users', function(req,res){
+//     var newContacts= {
+//         first_name : req.body.firstname,
+//         last_name: req.body.lastname,
+//         email: req.body.email,
+//         subject: req.body.subject,
+//         message: req.body.msg
+//     }
+//     console.log(newContacts);
+// });
+
 module.exports = router;
+
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+
+
+router.post('/', function (req, res, next) {
+   console.log('I AM AT USERS ROUTER');
+});
+
+
+// (accessed at POST http://localhost:3000/api/signup)
 
 module.exports = router;
